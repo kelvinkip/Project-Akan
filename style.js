f0533b997f39b4e383c554aac06aee3af8a5c9d1
kelvinkip <kelvinkipla@gmail.com>
2022-03-07 00:46:48 +0300

    refresh form after submission

---
+++ style.js
@@ -0,0 +1,9 @@
+// Function to refresh the form after submition
+function formRefresh(){
+    document.getElementById("form_data").reset();
+}
+
+// Event function on button
+function akanNames(){
+
+ 
